5ec7fc5863c909f8882e08da3de4f51c42598f24
Lars-Andre Scheiperpeter <lars-andre.scheiperpeter@edu.fhdw.de>
2026-06-02 15:09:32 +0200

    fix error

--- custom_components/smart_dhw/__init__.py
+++ custom_components/smart_dhw/__init__.py
@@ -1,3 +1,5 @@
+import importlib
+
 from homeassistant.core import HomeAssistant
 from .const import DOMAIN
 
@@ -8,5 +10,9 @@ async def async_setup(hass: HomeAssistant, config: dict):
 
 async def async_setup_entry(hass, entry):
     hass.data[DOMAIN][entry.entry_id] = entry.data
+    await hass.async_add_executor_job(
+        importlib.import_module,
+        "custom_components.smart_dhw.sensor",
+    )
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
